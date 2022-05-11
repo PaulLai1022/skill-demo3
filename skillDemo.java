674a480109dd3cc0f16426265a5818e3247b7d5b
PaulLai1022 <71100835+PaulLai1022@users.noreply.github.com>
2022-05-11 11:10:36 -0700

    lib + fail test

--- skillDemo.java
+++ skillDemo.java
@@ -1,1 +1,10 @@
-skillDemo{}
+public class skillDemo{
+    public static int subtraction(int a, int b){
+        return a-b;
+    }
+}
+
+@Test
+public void addition() {
+    assertEquals(2, 1 + 1);
+}
